b973ce436e8b077165a733202f9613960b247617
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2025-01-22 10:17:44 +0800

    1

--- test.js
+++ test.js
@@ -1,21 +1,14 @@
-const XLSX = require('xlsx');
-const fs = require('fs');
-const path = require('path');
 
-// 定义文件路径
-const Configurationspath = './Configurations.xlsx';
+const context1 = "\nBacklog6126\nHow: 活动—精选活动—WCS分地区显示大会页签&活动标题\nWhere: 主界面_活动\nWhat: 2025年WCS分地区赛事的页签&活动标题（Korea WCS Qualifier）\n"
+const context2 = "\nMcat6224\nHow: 1.打开对战指南-黎莫特竞技场（500分）\n2.选择页签2-1\n3.点击文柚果图标\n4.查看弹出的窗口中的文字描述\nWhere: 对战指南\nWhat: 对战指南描述文本\n"
 
-// 读取 Excel 文件并记录文件名
-function readExcel(filePath, fileName) {
-    const workbook = XLSX.readFile(filePath);
-    const sheetName = workbook.SheetNames[0];
-    const worksheet = workbook.Sheets[sheetName];
-    return XLSX.utils.sheet_to_json(worksheet).map(item => ({ ...item, "来源": fileName }));
-}
 
-// 读取七个 Excel 文件并记录文件名
-const ConfigurationsData = readExcel(Configurationspath, "Configurationspath");
 
-const USERID  = []
+// 提取 How: 之后的部分
+const howMatch1 = context1.match(/How:\s*(.*?)\s*Where:/);
+const howMatch2= context2.match(/How:\s*(.*?)\s*Where:/);
+const aa = howMatch1 ? howMatch1[1] : ''; 
+const bb = howMatch2 ? howMatch2[1] : ''; 
 
-console.log(ConfigurationsData);
+console.log(aa,'aa')
+console.log(bb,'bb')
