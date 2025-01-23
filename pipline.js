dfa9a2a63a37726456cd785b21e7b7e1f9c3d3fe
zhangcongcong <66811734+sorryzcc@users.noreply.github.com>
2025-01-23 15:45:58 +0800

    1

---
+++ pipline.js
@@ -0,0 +1,25 @@
+const XLSX = require('xlsx');
+const fs = require('fs');
+const path = require('path');
+
+// 定义文件路径
+const Configurationspath = '/data/landun/workspace/Configurations.xlsx';
+
+// 读取 Excel 文件并记录文件名
+function readExcel(filePath, fileName) {
+    const workbook = XLSX.readFile(filePath);
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+    return XLSX.utils.sheet_to_json(worksheet).map(item => ({ ...item, "来源": fileName }));
+}
+
+const ConfigurationsData = readExcel(Configurationspath, "Configurationspath");
+
+// 直接获取所有责任人的列表，包括重复项
+const allResponsibles = ConfigurationsData.map(item => item['责任人']).filter(Boolean).join(','); // 过滤掉可能存在的空值
+
+
+console.log(allResponsibles)
+
+// const allResponsibles = []; 
+setEnv('allResponsibles', `${allResponsibles}`);
